cdfc296bb92911395e21132f4614ce0b06b885b4
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-26 15:22:55 +0330

    feat: add generic ListView for list menuitems

--- menu_items/views.py
+++ menu_items/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import MenuItem
+
 
 # Create your views here.
+class MenuItemListView(generic.ListView):
+    model = MenuItem
+    template_name = 'menu_items/menu_item/sample.html'
