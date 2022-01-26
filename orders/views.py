b64d4b24dd821d6c5233b01adbb34f105bb094bc
Ali.es <ali.esmailnejad96@gmail.com>
2022-01-26 23:52:53 +0330

    feat: add OrderItemCreateView to order app

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import OrderItem, Order
+
 
 # Create your views here.
+class OrderItemCreateView(generic.CreateView):
+    model = OrderItem
+    fields = ['item', 'quantity', 'order']
+    template_name = 'orders/create_order_item.html'
+
+    success_url = '/create-order/'
+
